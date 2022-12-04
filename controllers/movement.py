23aa18d621a92e4120107dfcb10c4705898f9ecd
DensityInfinite <douglas_jiang@hotmail.com>
2022-12-04 15:39:52 +1100

    Added inputs and relative responses

--- controllers/movement.py
+++ controllers/movement.py
@@ -5,21 +5,21 @@ import navx
 class Movement:
     chassis: Chassis
     set_point = magicbot.tunable(0)
-    Kd = magicbot.tunable(0)
     Kp = magicbot.tunable(0)
+    Kd = magicbot.tunable(0)
 
-    def __init__(self):
+    def setup(self):
+        self.input_x = 0
+        self.input_y = 0
+        self.input_omega = 0
         self.last_error = self.chassis.get_rotation().radians()-self.set_point
 
     # take commanded speeds    
-    def set_inputs(self, vx, vy, omega):
-        ...
-    
-    def set_autoalign(self, value):
-        ...
-    
-    def set_field_oriented(self, value):
-        ...
+    def set_inputs(self, vx, vy, omega, a_button):
+        self.input_x = vx
+        self.input_y = vy
+        self.input_omega = omega
+        self.input_a = a_button
     
     def execute(self):
         # if autoalign compute rotation pid
@@ -29,4 +29,7 @@ class Movement:
         D = self.last_error-error
         controller_output = P + D
         self.last_error = error
-        self.chassis.drive_field(0, 0, controller_output)
+        if self.input_a:
+            self.chassis.drive_field(self.input_x, self.input_y, controller_output)
+        else:
+            self.chassis.drive_field(self.input_x, self.input_y, self.input_omega)
--- robot.py
+++ robot.py
@@ -13,11 +13,15 @@ class MyRobot(magicbot.MagicRobot):
 
     def createObjects(self) -> None:
         # create xbox controller
-        ...
+        self.gamepad = wpilib.XboxController(0)
     
     def teleopPeriodic(self) -> None:
         # read controller values and give them to movement
-        ...
+        x_input = self.gamepad.getLeftX()
+        y_input = self.gamepad.getLeftY()
+        omega = self.gamepad.getRightX()
+        a_input = self.gamepad.getAButton()
+        self.movement.set_inputs(x_input, y_input, omega, a_input)
         
 
 if __name__ == "__main__":
